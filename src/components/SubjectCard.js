8af964a3e0e2e5a9bb41bd84d2cb3ff5e3803038
ikennaoyiih@gmail.com <61995586+I-GiM@users.noreply.github.com>
2020-07-15 00:50:39 +0100

    feat: homepage

---
+++ src/components/SubjectCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/Style.css'
+
+const SubjectCard = (props) => {
+  return(
+    <React.Fragment>
+      <img className="img-fluid subject-img" src={props.image} onClick={props.goToSubject} alt=""/>
+      <span className="my-2 subject">{props.subject}</span>
+      <span className="my-2 topic-size">{props.topicSize}</span>
+    </React.Fragment>
+  )
+}
+
+export default SubjectCard
