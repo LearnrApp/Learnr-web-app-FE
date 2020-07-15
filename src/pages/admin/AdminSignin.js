fc548f64c75a2000623a39b86931a8e1440b65be
ikennaoyiih@gmail.com <61995586+I-GiM@users.noreply.github.com>
2020-07-15 11:51:42 +0100

    feat: parent-signup-signin-and-admin-signin

--- src/pages/admin/AdminSignin.js
+++ src/pages/admin/AdminSignin.js
@@ -1,22 +1,22 @@
 import React from 'react'
-import NavBar from '../../components/navbar/NavBar'
+import NavBar from '../../components/navbar/NavBarUser'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import '../../styles/Sign.css'
 import { Link } from 'react-router-dom'
 
-const StudentSignin = () => {
+const AdminSignin = () => {
   return (
     <React.Fragment>
       <NavBar />
-      <div className="sign-bg container-fluid px-0">
+      <div className="admin-bg container-fluid px-0">
         <div className="container py-5">
           <div className="mx-auto signup-form--wrap">
-          <p className="text-center">
-            Welcome Back
+          <p className="text-white text-center">
+            Administrator Sign In
           </p>
             <div className="bg-white signup-form">
-              <span className="text-center small mb-4 d-block">Sign in</span>
+              {/* <span className="text-center small mb-4 d-block">Sign in</span> */}
               <Form>
                 <Form.Group controlId="formBasicUsername">
                   <Form.Label className="small">Username *</Form.Label>
@@ -45,4 +45,4 @@ const StudentSignin = () => {
   )
 }
 
-export default StudentSignin
+export default AdminSignin
--- src/pages/parent/ParentSignin.js
+++ src/pages/parent/ParentSignin.js
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from '../../components/navbar/NavBar'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+// import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import '../../styles/Sign.css'
 import { Link } from 'react-router-dom'
 
@@ -16,7 +17,20 @@ const StudentSignin = () => {
             Welcome Back
           </p>
             <div className="bg-white signup-form">
-              <span className="text-center small mb-4 d-block">Sign in</span>
+              <span className="text-center mb-3 d-block">Sign in<br /><i>with one of these services</i></span>
+              <div className="row">
+                <Link className="col mr-2 d-flex align-items-center bg-white border rounded text-dark pr-4 py-2 my-3 d-block">
+                  <img className="mx-2" src={require('../../images/facebook-col.svg')} alt="" />
+                  Facebook
+                </Link>
+                <Link className="col ml-2 d-flex align-items-center bg-white border rounded text-dark pr-4 py-2 my-3 d-block">
+                  <img className="mx-2" src={require('../../images/google-col.svg')} alt="" />
+                  Google
+                </Link>
+              </div>
+              <div className="my-2 text-center">
+                <img src={require('../../images/line.svg')} alt="" /><span className="mx-4">or</span><img src={require('../../images/line.svg')} alt="" />
+              </div>
               <Form>
                 <Form.Group controlId="formBasicUsername">
                   <Form.Label className="small">Username *</Form.Label>
