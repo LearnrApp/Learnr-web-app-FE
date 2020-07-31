fb57d53a0f036cd2b85b987325694f12ae4ec323
ikennaoyiih@gmail.com <61995586+I-GiM@users.noreply.github.com>
2020-07-31 18:36:34 +0100

    refactor: Parent dashboard link

--- src/pages/parent/ParentDashboard.js
+++ src/pages/parent/ParentDashboard.js
@@ -3,24 +3,24 @@ import Helmet from 'react-helmet'
 import {Link} from  'react-router-dom'
 import SidebarItems from '../../components/Users/SidebarItems'
 import NavBarUser from '../../components/navbar/NavBarUser'
-import ParentCourses from '../../components/dashboard/parent/Courses/ParentCourses'
+import YourKids from '../../components/dashboard/parent/Courses/YourKids'
 import '../../styles/UserDashboard.css'
 import '../../styles/Style.css'
 
 const StudentDashboard = () => {
   const documentTitle = 'Learnr | Parent Dashboard'
 
-  const [dashboardMenu, updateMenu] = useState(<ParentCourses />)
+  const [dashboardMenu, updateMenu] = useState(<YourKids />)
 
-  const coursesLink = useRef()
+  const yourKidsLink = useRef()
   const achievementsLink = useRef()
   const helpLink = useRef()
   const profileLink = useRef()
   const subscriptionLink = useRef()
 
-  const updateBackgroundCourses = () => {
-    if(coursesLink.current) {
-      coursesLink.current.classList.add('sidebar--item--active')
+  const updateBackgroundYourKids = () => {
+    if(yourKidsLink.current) {
+      yourKidsLink.current.classList.add('sidebar--item--active')
       achievementsLink.current.classList.remove('sidebar--item--active')
       helpLink.current.classList.remove('sidebar--item--active')
       profileLink.current.classList.remove('sidebar--item--active')
@@ -30,7 +30,7 @@ const StudentDashboard = () => {
   const updateBackgroundAchievements = () => {
     if(achievementsLink.current) {
       achievementsLink.current.classList.add('sidebar--item--active')
-      coursesLink.current.classList.remove('sidebar--item--active')
+      yourKidsLink.current.classList.remove('sidebar--item--active')
       helpLink.current.classList.remove('sidebar--item--active')
       profileLink.current.classList.remove('sidebar--item--active')
       subscriptionLink.current.classList.remove('sidebar--item--active')
@@ -39,7 +39,7 @@ const StudentDashboard = () => {
   const updateBackgroundHelp = () => {
     if(helpLink.current) {
       helpLink.current.classList.add('sidebar--item--active')
-      coursesLink.current.classList.remove('sidebar--item--active')
+      yourKidsLink.current.classList.remove('sidebar--item--active')
       achievementsLink.current.classList.remove('sidebar--item--active')
       profileLink.current.classList.remove('sidebar--item--active')
       subscriptionLink.current.classList.remove('sidebar--item--active')
@@ -48,7 +48,7 @@ const StudentDashboard = () => {
   const updateBackgroundProfile = () => {
     if(profileLink.current) {
       profileLink.current.classList.add('sidebar--item--active')
-      coursesLink.current.classList.remove('sidebar--item--active')
+      yourKidsLink.current.classList.remove('sidebar--item--active')
       achievementsLink.current.classList.remove('sidebar--item--active')
       helpLink.current.classList.remove('sidebar--item--active')
       subscriptionLink.current.classList.remove('sidebar--item--active')
@@ -57,7 +57,7 @@ const StudentDashboard = () => {
   const updateBackgroundSubscription = () => {
     if(subscriptionLink.current) {
       subscriptionLink.current.classList.add('sidebar--item--active')
-      coursesLink.current.classList.remove('sidebar--item--active')
+      yourKidsLink.current.classList.remove('sidebar--item--active')
       achievementsLink.current.classList.remove('sidebar--item--active')
       helpLink.current.classList.remove('sidebar--item--active')
       profileLink.current.classList.remove('sidebar--item--active')
@@ -101,13 +101,13 @@ const StudentDashboard = () => {
           </div>
           <div className="pt-4">
             <div
-              ref={coursesLink}
+              ref={yourKidsLink}
               onClick={() => {
-              updateMenu(<ParentCourses />)
-              updateBackgroundCourses()
+              updateMenu(<YourKids />)
+              updateBackgroundYourKids()
             }}
             className="sidebar--item--active sidebar--item">
-              <SidebarItems sidebaricon={require('../../images/readme.svg')} sidebartext={'Courses'} />
+              <SidebarItems sidebaricon={require('../../images/user.svg')} sidebartext={'Your Kids'} />
             </div>
             <div
               ref={achievementsLink}
@@ -148,8 +148,39 @@ const StudentDashboard = () => {
             </div>
           </div>
         </div>
-      </div>
       <React.Fragment>{dashboardMenu}</React.Fragment>
+      </div>
+      <React.Fragment>
+      <div className="bg-white fixed-bottom bottom-navigate">
+        <div className="d-flex justify-content-between mx-2 mx-md-3 align-items-center">
+          <div onClick={() => {
+            updateMenu(<YourKids />)
+          }} className="sidebar--item small">
+            <SidebarItems sidebaricon={require('../../images/readme.svg')} sidebartext={'Courses'} />
+          </div>
+          <div onClick={() => {
+            // updateMenu(<QuizRecords />)
+          }} className="sidebar--item small">
+            <SidebarItems sidebaricon={require('../../images/loadbar-doc.svg')} sidebartext={'Quiz'} />
+          </div>
+          <div onClick={() => {
+            // updateMenu(<Achievements />)
+          }} className="sidebar--item small">
+            <SidebarItems sidebaricon={require('../../images/trophy.svg')} sidebartext={'Achievements'} />
+          </div>
+          <div onClick={() => {
+            // updateMenu(<HelpSupport />)
+          }} className="sidebar--item small">
+            <SidebarItems sidebaricon={require('../../images/help-circle.svg')} sidebartext={'Help'} />
+          </div>
+          <div onClick={() => {
+            // updateMenu(<Profile />)
+          }} className="sidebar--item small">
+            <SidebarItems sidebaricon={require('../../images/mdi_account_circle.svg')} sidebartext={'Profile'} />
+          </div>
+        </div>
+      </div>
+      </React.Fragment>
     </React.Fragment>
   )
 }
